4d035c286935a7b534e420d370c6a71fc642da51
Dmitry Gorbar <gorbar.dmitry@gmail.com>
2020-05-26 00:42:18 +0300

    commit latest changes

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker buid -t gorbar/multi-client:latest -t gorbar/multi-client:$SHA -f ./client/Dockerfile ./client
+docker buid -t gorbar/multi-server:latest -t gorbar/multi-server:$SHA -f ./server/Dockerfile ./server
+docker buid -t gorbar/multi-worker:latest -t gorbar/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push gorbar/multi-client:latest
+docker push gorbar/multi-server:latest
+docker push gorbar/multi-worker:latest
+docker push gorbar/multi-client:$SHA
+docker push gorbar/multi-server:$SHA
+docker push gorbar/multi-worker:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=gorbar/multi-client:$SHA
+kubectl set image deployments/server-deployment server=gorbar/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=gorbar/multi-worker:$SHA
