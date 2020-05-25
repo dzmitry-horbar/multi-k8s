cd6b4c084d534fc3b9e034ea5338fba2c002a60a
Dmitry Gorbar <gorbar.dmitry@gmail.com>
2020-05-26 01:45:21 +0300

    final fix

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
-docker buid -t gorbar/multi-client:latest -t gorbar/multi-client:$SHA -f ./client/Dockerfile ./client
-docker buid -t gorbar/multi-server:latest -t gorbar/multi-server:$SHA -f ./server/Dockerfile ./server
-docker buid -t gorbar/multi-worker:latest -t gorbar/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker buid -t gorbar/multi-client:latest -f ./client/Dockerfile ./client
+docker buid -t gorbar/multi-server:latest -f ./server/Dockerfile ./server
+docker buid -t gorbar/multi-worker:latest -f ./worker/Dockerfile ./worker
+docker buid -t gorbar/multi-client:$SHA -f ./client/Dockerfile ./client
+docker buid -t gorbar/multi-server:$SHA -f ./server/Dockerfile ./server
+docker buid -t gorbar/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push gorbar/multi-client:latest
 docker push gorbar/multi-server:latest
 docker push gorbar/multi-worker:latest
